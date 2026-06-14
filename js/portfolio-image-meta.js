02bf811786adb817c029e3ce22b7da5dacfdf6d2
Dan Xue <xueshots@gmail.com>
2026-06-14 17:59:36 -0400

    Add hidden portfolio gallery page

---
+++ js/portfolio-image-meta.js
@@ -0,0 +1,20 @@
+window.__IMAGE_META__ = window.__IMAGE_META__ || {};
+
+Object.assign(window.__IMAGE_META__, {
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-133_Web.jpg": { width: 1325, height: 2000 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-145_Web.jpg": { width: 1357, height: 2048 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-23_Web.jpg": { width: 2048, height: 1365 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-49_Web.jpg": { width: 2000, height: 1333 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-50_Web.jpg": { width: 1638, height: 2048 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-54_Web.jpg": { width: 1638, height: 2048 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-60_Web.jpg": { width: 1440, height: 1800 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-62_Web.jpg": { width: 1479, height: 2000 },
+  "assets/images/portfolio/2025_06_15_Travel_Iberia-87.jpg": { width: 3960, height: 4950 },
+  "assets/images/portfolio/20260516_Travel_Peru_0008_Web.jpg": { width: 1800, height: 1125 },
+  "assets/images/portfolio/20260516_Travel_Peru_0010_Web.jpg": { width: 1638, height: 2048 },
+  "assets/images/portfolio/20260517_Travel_Peru_0030_Web.jpg": { width: 2000, height: 1333 },
+  "assets/images/portfolio/20260517_Travel_Peru_0033_Web.jpg": { width: 1800, height: 1200 },
+  "assets/images/portfolio/20260517_Travel_Peru_0044_Web.jpg": { width: 1365, height: 2048 },
+  "assets/images/portfolio/20260518_Travel_Peru_0083_Web.jpg": { width: 2000, height: 1333 },
+  "assets/images/portfolio/Thailand2024-24 Web.jpg": { width: 2000, height: 1333 }
+});
